8c39be7f56a2f8148f59140090bbc5747844d800
vlaDick25 <111267065+vlaDick25@users.noreply.github.com>
2022-08-15 12:02:12 +0600

    1 сай html

---
+++ cod.java
@@ -0,0 +1,19 @@
+
+public class cod{
+    public static void main(String[] args){
+int x = 5;
+System.out.println("mi nimber is " + x);
+    }
+       
+
+        
+     
+        
+
+        
+    }
+
+
+
+    
+
